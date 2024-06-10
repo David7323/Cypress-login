a1819a2fdb650475db4e7c1c2d07e8f20b734299
David7323 <davidqs7323@gmail.com>
2024-06-10 11:27:47 -0300

    Add page objects

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -1,12 +1,21 @@
 import userData from '../fixtures/user-data.json'
+import LoginPage from '../pages/loginPage.js'
+import dashboardPage from '../pages/dashboardPage.js'
+import menuPage from '../pages/menuPage.js'
 
+
+const loginPage = new LoginPage()
+
+const dashboard = new dashboardPage()
+
+const menu = new menuPage()
 
 describe('Orange HRM Test', () => {
 
 const selectorList = {
-  usernameField: "[name='username']",
-  passwordField: "[name='password']",
-  loginButton: "[type='submit']",
+  // usernameField: "[name='username']",
+  // passwordField: "[name='password']",
+  // loginButton: "[type='submit']",
   sectionTitleTopBar: ".oxd-topbar-header-breadcrumb > .oxd-text",
   wrongCredentialAlert: ".oxd-alert",
   dashboardGrid: ".orangehrm-dashboard-grid",
@@ -15,19 +24,25 @@ const selectorList = {
   lastNameField: "[name='lastName']",
   genericField: ".oxd-input--active",
   dateField: "[placeholder='dd-mm-yyyy']",
+  genericCombobox: ".oxd-select-text--arrow",
   dateCloseButton: '.--close',
-  submitButton: "[type='submit']"
+  submitButton: "[type='submit']",
 } 
   it.only('User Info Update - Success', () => {
-    cy.visit('/auth/login')
-    cy.get(selectorList.usernameField).type(userData.userSuccsess.username)
-    cy.get(selectorList.passwordField).type(userData.userSuccsess.password)
-    cy.get(selectorList.loginButton).click()
-    cy.location('pathname').should('equal' , '/web/index.php/dashboard/index')
-    cy.get(selectorList.dashboardGrid)
-    cy.get(selectorList.myInfoButton).click()
+    loginPage.accessLoginPage()
+    loginPage.loginWithUAnySer(userData.userSuccsess.username, userData.userSuccsess.password)
+    // cy.visit('/auth/login')
+    // cy.get(selectorList.usernameField).type(userData.userSuccsess.username)
+    // cy.get(selectorList.passwordField).type(userData.userSuccsess.password)
+    // cy.get(selectorList.loginButton).click()
+    dashboard.checkdashboardPage()
+    //cy.location('pathname').should('equal' , '/web/index.php/dashboard/index')
+    //cy.get(selectorList.dashboardGrid)
+    menu.accessMyInfo()
+    //cy.get(selectorList.myInfoButton).click()
     cy.get(selectorList.firstNameField).clear().type('David')
     cy.get(selectorList.lastNameField).clear().type('Silva')
+    //cy.get(selectorList.genericField).eq(3).clear().type('NickName')
     cy.get(selectorList.genericField).eq(3).clear().type('Employee')
     cy.get(selectorList.genericField).eq(4).clear().type('otherIdTest')
     cy.get(selectorList.genericField).eq(5).clear().type('Drivers License')
@@ -36,6 +51,20 @@ const selectorList = {
     cy.get(selectorList.submitButton).eq(1).click()
     cy.get('body').should('contain','Successfully Saved')
     cy.get('.oxd-toast-close')
+    cy.get(selectorList.genericCombobox).eq(0).click({ force: true})
+    cy.get(':nth-child(27) > span').click()
+    cy.get(selectorList.genericCombobox).eq(1).click({ force: true})
+    cy.get('.oxd-select-dropdown > :nth-child(3)').click()
+    cy.get(selectorList.genericField).eq(8).clear().type('1987-22-12')
+    cy.get(selectorList.dateCloseButton).click()
+
+
+
+
+
+
+
+   
 
     //cy.get(selectorList.genericField).eq(8).clear().type('snnNumberTest')
     //cy.get(selectorList.genericField).eq(9).clear().type('SinNumberTest')
---
+++ cypress/pages/loginPage.js
@@ -0,0 +1,26 @@
+class LoginPage {
+
+  selectorList()  {
+    const selectors = {
+        usernameField: "[name='username']",
+        passwordField: "[name='password']",
+        loginButton: "[type='submit']",
+        wrongCredentialAlert: ".oxd-alert",
+    }
+    return selectors
+  }
+
+  accessLoginPage() {
+    cy.visit('/auth/login')
+  }
+
+  loginWithUAnySer(username, password) {
+    cy.get(this.selectorList().usernameField).type(username)
+    cy.get(this.selectorList().passwordField).type(password)
+    cy.get(this.selectorList().loginButton).click()
+
+  }
+
+}      
+
+export default LoginPage
